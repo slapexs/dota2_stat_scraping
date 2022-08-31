d940b73cfc641b58bc1b1c196ad5105cbd045dd7
onze <slapexs@gmail.com>
2022-08-31 23:43:54 +0700

    Clean data and append to list

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from operator import index
 import requests
 from bs4 import BeautifulSoup
 
@@ -6,3 +7,20 @@ res_data = requests.get(url)
 
 # Select data
 raw_data = BeautifulSoup(res_data.text, 'html.parser')
+
+# Find data from html tag
+table = raw_data.find('table')
+tr = table.find_all('tr', {'class':'dota-stat-row'})
+
+def findDataFromTr(trList):
+    td = trList.find_all('td')
+    temp = []
+    for index in range(len(td)):
+        temp.append(td[index].text.strip())
+    return temp
+
+for data in tr:
+    response = findDataFromTr(data)
+    
+
+
