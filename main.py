ddaa140d11823b26e884a699d85f813391a80604
onze <slapexs@gmail.com>
2022-08-31 23:24:46 +0700

    Get raw data from website with bs4

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import requests
+from bs4 import BeautifulSoup
 
 url = 'https://liquipedia.net/dota2/ESL_One/Malaysia/2022/Statistics'
 res_data = requests.get(url)
-print(res_data)
+
+# Select data
+raw_data = BeautifulSoup(res_data.text, 'html.parser')
