3353fac9a14718810f79185edfc837b6c30b67b8
onze <slapexs@gmail.com>
2022-08-31 23:53:14 +0700

    Append data into list in Picks section

--- main.py
+++ main.py
@@ -19,8 +19,20 @@ def findDataFromTr(trList):
         temp.append(td[index].text.strip())
     return temp
 
+hero_name = []
+# List of data picks
+pick_played = []
+pick_win = []
+pick_lost = []
+pick_winrate = []
+pick_teamplayed = []
+
+# Append data into list
 for data in tr:
     response = findDataFromTr(data)
-    
-
-
+    hero_name.append(response[1].strip())
+    pick_played.append(int(response[2]))
+    pick_win.append(int(response[3]))
+    pick_lost.append(int(response[4]))
+    pick_winrate.append(float(response[5].split('%')[0]) if response[5].split('%')[0] != '-' else 0)
+    pick_teamplayed.append(float(response[6].split('%')[0]) if response[6].split('%')[0] != '-' else 0)
